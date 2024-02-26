6aeb303ef63642215a7931e961bfd70b8e68bd1c
Rizqy H <rizqy@dicoding.com>
2024-02-26 15:15:15 +0700

    use formatted date time for filename

--- src/utils.ts
+++ src/utils.ts
@@ -13,5 +13,16 @@ export function slugify(str: string) {
 }
 
 export function generateVideoFilename(basename: string) {
-  return `${slugify(basename)}-${new Date().valueOf()}.webm`;
+  const currentDate = new Date();
+
+  // Extract date components
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+  const dateTime = `${year}${month}${day}_${hours}${minutes}${seconds}`;
+
+  return `${slugify(basename)}_${dateTime}.webm`;
 }
