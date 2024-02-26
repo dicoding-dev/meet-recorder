862db67a1ff737df6e0fe32ef460b385a819e2b1
Rizqy H <rizqy@dicoding.com>
2024-02-26 15:55:11 +0700

    add output directory as CLI argument

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,31 @@ import "dotenv/config";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { recordMeeting } from "./recorder";
+import fs from "fs";
+import path from "path";
 
-const args = yargs(hideBin(process.argv))
+const argv = yargs(hideBin(process.argv))
   .option("url", {
     alias: "u",
     describe: "The meeting URL",
     demandOption: true,
   })
+  .option("outDir", {
+    alias: "o",
+    default: "../videos",
+    describe:
+      "The path to directory that will be used to store the recorded video",
+  })
+  .middleware(function (argv) {
+    argv.outDir = argv.o = path.resolve(__dirname, argv.outDir);
+  }, true)
   .check((argv) => {
     if (!argv.url.startsWith("https://meet.google.com")) {
       throw new Error("Invalid Google Meet URL");
+    }
+
+    if (!fs.existsSync(argv.outDir)) {
+      throw new Error("The output directory path doesn't exist");
     }
 
     return true;
@@ -22,5 +37,5 @@ const args = yargs(hideBin(process.argv))
   const googleAccount = process.env.GOOGLE_EMAIL as string;
   const googlePassword = process.env.GOOGLE_PASSWORD as string;
 
-  await recordMeeting(args.url, googleAccount, googlePassword);
+  await recordMeeting(argv.url, googleAccount, googlePassword);
 })();
